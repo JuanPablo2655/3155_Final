5a532aaafe93ce44fa8355331dd8f5e1a06d017f
Santiago <santiagodubon@outlook.com>
2022-10-26 19:33:31 -0400

    Added routes in app.py for community and user

--- app.py
+++ app.py
@@ -6,3 +6,13 @@ app = Flask(__name__)
 @app.get("/")
 def index():
     return render_template("index.html")
+
+# COMMUNITY POSTS / FEED
+@app.get("/community")
+def community():
+    return None
+
+# USER PROFILE
+@app.get("/user")
+def user():
+    return None
