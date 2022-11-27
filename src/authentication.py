aa20d59299f727a0339eda34be8ba019fa8d8901
JuanPablo2655 <personal@isidrocasiano.com>
2022-11-27 13:59:31 -0500

    fix: import bug

--- src/authentication.py
+++ src/authentication.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for, session
-from security import bcrypt
-from config import bcrypt_rounds
+from src.security import bcrypt
+from src.config import bcrypt_rounds
 
 from src.database.account import account
 
