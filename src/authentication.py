64ae9e68405e8671145af60134d52f3588f0495d
Isidro Casiano <personal@isidrocasiano.com>
2022-11-30 17:12:09 -0500

    feat: check if confirm password is equal to password

--- src/authentication.py
+++ src/authentication.py
@@ -39,23 +39,26 @@ def register():
         flash('already logged in', category='error')
         return redirect('/')
     else:
-        if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form and 'confirm_password' in request.form:
             fullname = request.form.get('fullname')
             email = request.form.get('email')
             user_name = request.form.get('username')
             password = request.form.get('password')
+            confirm_password = request.form.get('confirm_password')
 
             # salt and hash the password
-            hashed_bytes = bcrypt.generate_password_hash(
-                password, bcrypt_rounds)
-            hashed_password = hashed_bytes.decode('utf-8')
 
             # Check for different conditions, such as if the database has an email/username registered. If not, then make account.
             if account.check_username(user_name):
                 flash("Someone has this username", category='error')
             elif account.check_email(email):
                 flash("Someone has taken this email", category='error')
+            elif password != confirm_password:
+                flash("Passwords are not the same", category='error')
             else:
+                hashed_bytes = bcrypt.generate_password_hash(
+                    password, bcrypt_rounds)
+                hashed_password = hashed_bytes.decode('utf-8')
                 flash("Registered Successfully!")
                 new_account = account.create_user_account(
                     user_name, fullname, hashed_password, email)
