6ff657d283c26d5ff364ad6d548c154d2671e6b8
Christian <acostacastanonchristian@gmail.com>
2022-12-04 14:56:34 -0500

    Adding sqlalchemy and minor changes

--- src/database/comment.py
+++ src/database/comment.py
@@ -7,19 +7,18 @@ class Comment:
         comment = CommentModel.query.filter_by(comment_id=comment_id).first()
         return comment
     
-    def create_comment(author, date_posted, comment, post_id, account_id): 
+    def create_comment(author, date_posted, content, post_id, account_id): 
         comment = CommentModel(author=author, date_posted=date_posted, content=content, post_id=post_id, account_id=account_id)
         db.session.add(comment)
         db.session.commit()
 
-    def update_comment(content):
+    def update_comment(comment, content):
         comment.content = content 
         db.session.commit() 
 
     def delete_comment(comment): 
         db.session.delete(comment) 
         db.session.commit()
-
 
 
 comment = Comment() 
--- src/database/post.py
+++ src/database/post.py
@@ -9,6 +9,18 @@ class Post:
 
         #TODO: Add more methods to the post. Delete when done 
 
+    def create_post(title, content, account_id): 
+        post = Post(title=title, content=content, account_id=account_id)
+        db.session.add(post)
+        db.session.commit()
+
+    def update_post(post, description): 
+        post.description =  description
+        db.session.commit() 
+
+    def delete_post(post): 
+        db.session.delete(post)
+        db.session.commit()
 
 
 
