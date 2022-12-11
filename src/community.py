1b7961a1d957c61188ffa7c79299dc7ebf1f97e9
JuanPablo2655 <personal@isidrocasiano.com>
2022-12-11 15:58:56 -0500

    feat: validate create post

--- src/community.py
+++ src/community.py
@@ -8,11 +8,13 @@ community_blueprint = Blueprint('community', __name__)
 @community_blueprint.get('/communities')
 def communities():
     all_communities = community_db.get_all_communities()
-    return render_template('communities.html', communities = all_communities)
+    return render_template('communities.html', communities=all_communities)
+
 
 @community_blueprint.route('/community/form', methods=['GET', 'POST'])
 def create_community_form():
     return render_template('create_community.html')
+
 
 @community_blueprint.route('/community/create', methods=['GET', 'POST'])
 def create_community():
@@ -21,11 +23,10 @@ def create_community():
         description = request.form.get('description')
         if name == '' or description == '':
             abort(400)
-        new_community = community_db.create_community(name, description)
+        community_db.create_community(name, description)
         return redirect(url_for('community.communities'))
 
     return render_template('communities.html')
-     
 
 
 @community_blueprint.route("/community/<string:name>", methods=['GET', 'POST'])
@@ -40,13 +41,16 @@ def community(name):
 def create_post(name):
     community_obj = community_db.get_community(name)
 
-    if request.method == 'POST': 
+    if request.method == 'POST':
         title = request.form.get('title')
-        content = request.form.get('description') 
+        content = request.form.get('description')
         author = session['user']['user_name']
         account_id = session['user']['user_id']
-        community_id = community_obj.community_id 
-        post1 = post_db.create_post(title, content, author, community_id, account_id)
+        community_id = community_obj.community_id
+        if title == '' or content == '' or 'user' not in session:
+            abort(400)
+        post_db.create_post(
+            title, content, author, community_id, account_id)
         return redirect(url_for('community.communities'))
 
     return render_template('create.html', community=community_obj)
