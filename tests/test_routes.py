762eda1ef24f77160cc40702d6b47eab5abb863f
JuanPablo2655 <personal@isidrocasiano.com>
2022-12-13 16:45:02 -0500

    fix: test

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -3,6 +3,7 @@ from flask.testing import FlaskClient
 from src.models import Community
 from tests.utils import refresh_db, create_community
 
+
 def test_get_all_communities(test_app: FlaskClient):
     refresh_db()
     test_community = create_community()
@@ -11,8 +12,9 @@ def test_get_all_communities(test_app: FlaskClient):
     page_data: str = response.data.decode()
 
     assert response.status_code == 200
-    assert f'<a class="text-2xl text-blue-700" href="/community/{test_community.community_name}">Fortnite</a>' in page_data
-    assert '<p class="text-white">Super awesome epic game I love Fortnite!</p>' in page_data
+    assert f'Fortnite' in page_data
+    assert 'Super awesome epic game I love Fortnite!' in page_data
+
 
 def test_no_communities(test_app: FlaskClient):
     refresh_db()
@@ -25,7 +27,6 @@ def test_no_communities(test_app: FlaskClient):
     assert '<p>' not in page_data
 
 
-
 def test_get_single_community(test_app: FlaskClient):
     refresh_db()
     test_community = create_community()
@@ -34,8 +35,5 @@ def test_get_single_community(test_app: FlaskClient):
     page_data: str = response.data.decode()
 
     assert response.status_code == 200
-    assert f'<h1 class="text-center text-lg font-bold">{test_community.community_name}</h1>' in page_data
+    assert f'{test_community.community_name}' in page_data
     assert '<button action="submit">Submit</button>' in page_data
-    
-
-    
