adb55a3387d0c6ad4ef69610eaad02c4e739295c
JuanPablo2655 <personal@isidrocasiano.com>
2022-12-14 03:11:23 -0500

    feat: slug

--- src/community.py
+++ src/community.py
@@ -15,17 +15,17 @@ def communities():
 
 @community_blueprint.route('/community/form', methods=['GET', 'POST'])
 def create_community_form():
-    if 'user' not in session: 
+    if 'user' not in session:
         return redirect('/login')
-    else: 
+    else:
         return render_template('create_community.html')
 
 
 @community_blueprint.route('/community/create', methods=['GET', 'POST'])
 def create_community():
     if request.method == 'POST':
-        name = request.form.get('name')
-        no_space_name = ''.join(name.split())
+        name = str(request.form.get('name')).strip()
+        slug = ''.join(name.split())
         description = request.form.get('description')
         community_object = community_db.check_community(name)
         account_id = session['user']['user_id']
@@ -36,155 +36,162 @@ def create_community():
         else:
             if name == '' or description == '':
                 abort(400)
-        community_db.create_community(no_space_name, description, account_id)
+        community_db.create_community(slug, name, description, account_id)
         return redirect(url_for('community.communities'))
     else:
         return render_template('communities.html')
 
-#Name is community name
-@community_blueprint.route("/community/<string:name>", methods=['GET', 'POST'])
-def community(name):
+# Name is community name
 
-    community_obj = community_db.get_community(name)
-    get_all_posts = post_db.get_posts(name)
 
+@community_blueprint.route("/community/<string:slug>", methods=['GET', 'POST'])
+def community(slug):
+
+    community_obj = community_db.get_community(slug)
+    get_all_posts = post_db.get_posts(slug)
 
     return render_template('community.html', community=community_obj, posts=get_all_posts)
 
 # create a post
 
 
-@community_blueprint.route('/community/<string:name>/post', methods=['GET', 'POST'])
-def create_post(name):
-    community_obj = community_db.get_community(name)
+@community_blueprint.route('/community/<string:slug>/post', methods=['GET', 'POST'])
+def create_post(slug):
+    community_obj = community_db.get_community(slug)
 
-    if 'user' not in session: 
+    if 'user' not in session:
         return redirect('/login')
-    
+
     if request.method == 'POST' and 'user' in session:
         title = request.form.get('title')
         content = request.form.get('description')
         author = session['user']['username']
         account_id = session['user']['user_id']
-        community_name = name
+        community_slug = slug
+        community_name = community_obj.community_name
         community_id = community_obj.community_id
 
         if title == '' or content == '' or 'user' not in session:
             abort(400)
         post_db.create_post(
-            title, author, content, community_name, account_id, community_id)
-        return redirect(url_for('community.community', name=name))
+            title, author, content, community_slug, community_name, account_id, community_id)
+        return redirect(url_for('community.community', slug=slug))
 
     return render_template('create.html', community=community_obj)
 
-@community_blueprint.post('/community/<string:name>/<int:post_id>/delete')
-def delete_post(post_id, name): 
-    if 'user' not in session: 
+
+@community_blueprint.post('/community/<string:slug>/<int:post_id>/delete')
+def delete_post(post_id, slug):
+    if 'user' not in session:
         return redirect('/login')
-    else: 
+    else:
         post = post_db.get_post(post_id)
-        if post: 
-            if post.account_id != session['user']['user_id']: 
-                return redirect(url_for('community.get_specific_post', name=post.community_name, post_id=post.post_id))
+        if post:
+            if post.account_id != session['user']['user_id']:
+                return redirect(url_for('community.get_specific_post', slug=post.community_slug, post_id=post.post_id))
             post_db.delete_post(post)
             flash("Successfully deleted post.", "success")
             return redirect('/')
-        else: 
+        else:
             abort(404)
 
-@community_blueprint.route('/community/<string:name>/<int:post_id>', methods=['GET'])
-def get_specific_post(name, post_id): 
-    community_obj = community_db.get_community(name)
+
+@community_blueprint.route('/community/<string:slug>/<int:post_id>', methods=['GET'])
+def get_specific_post(slug, post_id):
+    community_obj = community_db.get_community(slug)
     post = post_db.get_post(post_id)
-    if name != post.community_name:
+    if slug != post.community_slug:
         abort(400, 'Bad request')
-    else: 
+    else:
         get_all_comments = comment_db.get_all_comment_from_post(post_id)
-    
+
     return render_template('post.html', community=community_obj, post=post, comments=get_all_comments)
 
-@community_blueprint.route('/community/<string:name>/<int:post_id>', methods=['POST'])
-def create_comment(name, post_id): 
-    post_obj = post_db.get_post(post_id)
-    community_obj = community_db.get_community(name)
 
-    if request.method == 'POST' and 'user' in session: 
+@community_blueprint.route('/community/<string:slug>/<int:post_id>', methods=['POST'])
+def create_comment(slug, post_id):
+    post_obj = post_db.get_post(post_id)
+    community_obj = community_db.get_community(slug)
+
+    if request.method == 'POST' and 'user' in session:
         content = request.form.get('description')
         author = session['user']['username']
         post_id = post_obj.post_id
         account_id = session['user']['user_id']
-        new_comment = comment_db.create_new_comment(author, content, post_id, account_id)
-        return redirect(f'/community/{community_obj.community_name}/{post_id}')
-    else: 
+        new_comment = comment_db.create_new_comment(
+            author, content, post_id, account_id)
+        return redirect(f'/community/{community_obj.slug}/{post_id}')
+    else:
         return redirect('/login')
 
-@community_blueprint.route('/community/<string:name>/<int:post_id>/edit', methods=['GET', 'POST'])
-def edit_post(name, post_id): 
-    if 'user' not in session: 
+
+@community_blueprint.route('/community/<string:slug>/<int:post_id>/edit', methods=['GET', 'POST'])
+def edit_post(slug, post_id):
+    if 'user' not in session:
         redirect('/login')
-    else: 
+    else:
         post = post_db.get_post(post_id)
-        if request.method == 'POST' and 'user' in session and post.account_id == session['user']['user_id']: 
+        if request.method == 'POST' and 'user' in session and post.account_id == session['user']['user_id']:
             title = request.form.get('title')
             content = request.form.get('content')
             post_db.update_post(post, title, content)
-            return redirect(url_for('community.get_specific_post', name=name, post_id=post_id))
+            return redirect(url_for('community.get_specific_post', slug=slug, post_id=post_id))
     return render_template('edit_post.html', post=post)
 
-@community_blueprint.post('/community/<string:name>/<int:post_id>/comment/<int:comment_id>/delete')
-def delete_comment(name, post_id, comment_id): 
+
+@community_blueprint.post('/community/<string:slug>/<int:post_id>/comment/<int:comment_id>/delete')
+def delete_comment(slug, post_id, comment_id):
     post = post_db.get_post(post_id)
-    if 'user' not in session: 
+    if 'user' not in session:
         return redirect('/login')
-    else: 
+    else:
         comment = comment_db.get_comment(comment_id)
-    if comment: 
+    if comment:
         if comment.account_id != session['user']['user_id']:
-            return redirect(url_for('community.get_specific_post', name=post.community_name, post_id=post.post_id))
+            return redirect(url_for('community.get_specific_post', slug=post.community_slug, post_id=post.post_id))
         comment_db.delete_comment(comment)
         flash("Successfully deleted post.", "success")
-        return redirect(f'/community/{post.community_name}/{post_id}')
-    else: 
+        return redirect(f'/community/{post.community_slug}/{post_id}')
+    else:
         abort(404)
 
-@community_blueprint.route('/community/<string:name>/<int:post_id>/comment/<int:comment_id>/edit', methods=['GET', 'POST'])
-def edit_comment(name, post_id, comment_id): 
+
+@community_blueprint.route('/community/<string:slug>/<int:post_id>/comment/<int:comment_id>/edit', methods=['GET', 'POST'])
+def edit_comment(slug, post_id, comment_id):
     post = post_db.get_post(post_id)
-    if 'user' not in session: 
+    if 'user' not in session:
         redirect('/login')
-    else: 
+    else:
         comment = comment_db.get_comment(comment_id)
-        if request.method == 'POST' and 'user' in session and comment.account_id == session['user']['user_id']: 
+        if request.method == 'POST' and 'user' in session and comment.account_id == session['user']['user_id']:
             content = request.form.get('content')
             comment_db.update_comment(comment, content)
-            return redirect(url_for('community.get_specific_post', name=name, post_id=comment.post_id))
+            return redirect(url_for('community.get_specific_post', slug=slug, post_id=comment.post_id))
     return render_template('edit_comment.html', comment=comment, post=post)
 
-@community_blueprint.route('/community/<string:name>/edit', methods=['GET', 'POST'])
-def edit_community(name): 
-    community=community_db.check_community(name)
-    if 'user' not in session: 
+
+@community_blueprint.route('/community/<string:slug>/edit', methods=['GET', 'POST'])
+def edit_community(slug):
+    community = community_db.check_community(slug)
+    if 'user' not in session:
         redirect('/login')
-    else: 
-        community=community_db.check_community(name)
-        if request.method == 'POST' and 'user' in session and community.account_id == session['user']['user_id']: 
+    else:
+        community = community_db.check_community(slug)
+        if request.method == 'POST' and 'user' in session and community.account_id == session['user']['user_id']:
             description = request.form.get('description')
             community_db.update_community(community, description)
             return redirect('/communities')
     return render_template('edit_community.html', community=community)
 
-@community_blueprint.post('/community/<string:name>/delete')
-def delete_community(name):
-    if 'user' not in session: 
+
+@community_blueprint.post('/community/<string:slug>/delete')
+def delete_community(slug):
+    if 'user' not in session:
         return redirect('/login')
-    else: 
-        community = community_db.check_community(name) 
-    if community: 
-        if community.account_id != session['user']['user_id']: 
+    else:
+        community = community_db.check_community(slug)
+    if community:
+        if community.account_id != session['user']['user_id']:
             return redirect('/communities')
         community_db.delete_community(community)
         return redirect('/communities')
-
-
-
-    
--- src/database/post.py
+++ src/database/post.py
@@ -3,27 +3,30 @@ from src.models import db, Post as PostModel, Community
 
 class Post:
 
-    #Gets all the posts regarding to the community_name 
-    def get_posts(self, community_name) -> list[PostModel]:
-        posts = PostModel.query.join(Community, PostModel.community_id == Community.community_id).filter(Community.community_name == community_name).all()
+    # Gets all the posts regarding to the slug
+    def get_posts(self, slug) -> list[PostModel]:
+        posts = PostModel.query.join(Community, PostModel.community_id == Community.community_id).filter(
+            Community.slug == slug).all()
         return posts
 
-    def get_post(self, post_id): 
+    def get_post(self, post_id):
         post = PostModel.query.filter(PostModel.post_id == post_id).first()
-        return post 
+        return post
 
-    def get_four_posts(self): 
-        recent_posts = PostModel.query.order_by(PostModel.post_id.desc()).limit(4).all()
+    def get_four_posts(self):
+        recent_posts = PostModel.query.order_by(
+            PostModel.post_id.desc()).limit(4).all()
         return recent_posts
 
         # TODO: Add more methods to the post. Delete when done
-    def create_post(self, title, author, content, community_name, community_id, account_id): 
-        post = PostModel(title, author, content, community_name, community_id, account_id) 
+    def create_post(self, title, author, content, community_slug, community_name, community_id, account_id):
+        post = PostModel(title, author, content,
+                         community_slug, community_name, community_id, account_id)
         db.session.add(post)
         db.session.commit()
 
     def update_post(self, post, title, content):
-        post.title = title 
+        post.title = title
         post.content = content
         db.session.commit()
 
