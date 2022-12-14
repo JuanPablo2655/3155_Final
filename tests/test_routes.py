1eeab834c53cad18c73b0bfd151ba1c599d27a30
Zach_Haynie <zhaynie@uncc.edu>
2022-12-13 21:35:31 -0500

    Tests for posts done

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -1,12 +1,8 @@
 from flask.testing import FlaskClient
 
 from src.models import Community
-from tests.utils import refresh_db, create_community
+from tests.utils import refresh_db, create_community, create_user, create_post
 
-<<<<<<< HEAD
-=======
-
->>>>>>> e77c14f99d255543eceea12b58a032b5839b3c6d
 def test_get_all_communities(test_app: FlaskClient):
     refresh_db()
     test_community = create_community()
@@ -15,14 +11,9 @@ def test_get_all_communities(test_app: FlaskClient):
     page_data: str = response.data.decode()
 
     assert response.status_code == 200
-<<<<<<< HEAD
-    assert f'<a class="text-2xl text-blue-700" href="/community/{test_community.community_name}">Fortnite</a>' in page_data
-    assert '<p class="text-white">Super awesome epic game I love Fortnite!</p>' in page_data
-=======
-    assert f'Fortnite' in page_data
+    assert 'Fortnite' in page_data
     assert 'Super awesome epic game I love Fortnite!' in page_data
 
->>>>>>> e77c14f99d255543eceea12b58a032b5839b3c6d
 
 def test_no_communities(test_app: FlaskClient):
     refresh_db()
@@ -31,13 +22,7 @@ def test_no_communities(test_app: FlaskClient):
     page_data: str = response.data.decode()
 
     assert response.status_code == 200
-    assert '<a>' not in page_data
-<<<<<<< HEAD
-    assert '<p>It looks empty, lets make a community!</p>' in page_data
-
-=======
-    assert '<p>' not in page_data
->>>>>>> e77c14f99d255543eceea12b58a032b5839b3c6d
+    assert 'Fortnite' not in page_data
 
 
 def test_get_single_community(test_app: FlaskClient):
@@ -48,13 +33,30 @@ def test_get_single_community(test_app: FlaskClient):
     page_data: str = response.data.decode()
 
     assert response.status_code == 200
-<<<<<<< HEAD
-    assert f'<h1 class="text-center text-lg font-bold">{test_community.community_name}</h1>' in page_data
-    assert '<button action="submit">Submit</button>' in page_data
-    
+    assert 'Fortnite' in page_data
 
-    
-=======
-    assert f'{test_community.community_name}' in page_data
-    # assert '<button action="submit">Submit</button>' in page_data
->>>>>>> e77c14f99d255543eceea12b58a032b5839b3c6d
+def test_get_post(test_app: FlaskClient):
+    refresh_db()
+    test_community = create_community()
+    test_user = create_user()
+    test_post = create_post(community_id=test_community.community_id, account_id=test_user.account_id)
+
+    response = test_app.get(f'/community/{test_community.community_name}')
+    page_data: str = response.data.decode()
+
+    assert response.status_code == 200
+    assert 'Fortnite' in page_data
+    assert 'xxflash' in page_data
+    assert 'I love this season' in page_data
+
+def get_no_posts(test_app: FlaskClient):
+    test_community = create_community()
+    test_user = create_user()
+
+    response = test_app.get(f'/community/{test_community.community_name}')
+    page_data: str = response.data.decode()
+
+    assert 'Fortnite' not in page_data
+    assert 'xxflash' not in page_data
+    assert 'I love this season' in page_data
+
--- tests/utils.py
+++ tests/utils.py
@@ -1,17 +1,4 @@
-<<<<<<< HEAD
-from src.models import Community, db
-
-def refresh_db():
-    Community.query.delete()
-    db.session.commit()
-
-def create_community(name = 'Fortnite', description = 'Super awesome epic game I love Fortnite!')-> Community:
-    test_community = Community(community_name = name, description = description)
-    db.session.add(test_community)
-    db.session.commit()
-    return test_community
-=======
-from src.models import Community, Post, Comment, db
+from src.models import Community, Post, Comment, Account, db
 
 
 def refresh_db():
@@ -26,4 +13,15 @@ def create_community(name='Fortnite', description='Super awesome epic game I lov
     db.session.add(test_community)
     db.session.commit()
     return test_community
->>>>>>> e77c14f99d255543eceea12b58a032b5839b3c6d
+
+def create_user(user_name='xxflash', full_name='Barry Allen', gaming_password = 'Flash01', email = 'bAllen@gmail.com') -> Account:
+    test_user = Account(user_name=user_name, full_name=full_name, gaming_password = gaming_password, email = email)
+    db.session.add(test_user)
+    db.session.commit()
+    return test_user
+
+def create_post(title = 'Fortnite C4', author = 'xxflash', content = 'I love this season', community_name = 'Fortnite', community_id:int =1, account_id:int = 1) -> Post:
+    test_post = Post(title = title, author = author, content = content, community_name = community_name, community_id= community_id, account_id = account_id)
+    db.session.add(test_post)
+    db.session.commit()
+    return test_post
